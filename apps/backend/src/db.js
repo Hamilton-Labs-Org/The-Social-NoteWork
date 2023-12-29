63ffa4bb266c25f37e9df5fca585118dfe36c65e
Terence P. Hamilton <24661651+HamiltonMultimedia@users.noreply.github.com>
2023-12-29 14:50:55 -0500

    Database (#3)
    * new database branch
    
    * new database branch
    
    * new database branch
    
    * new database branch
    
    * 🔋 Connected mongoose to mongodb
    
    * 🔋 Connected mongoose to mongodb
    
    * 🔋 Connected mongoose to mongodb
    
    * 🚨 Added ESLint@Google style w/Prettier script
    
    * 🚨 Updated GitHub CI Lint action
    
    * 🚨 Updated GitHub CI Lint action
    
    * 🚨 Updated GitHub CI Lint action
    
    * 🚨 Updated GitHub CI Lint action
    
    * 📝 Updated README
    
    * 📝 Updated README

---
+++ apps/backend/src/db.js
@@ -0,0 +1,41 @@
+// Require the mongoose library
+import mongoose from 'mongoose';
+
+export default {
+	connect: (DB_HOST) => {
+		mongoose.connect(DB_HOST).then(
+			() => {
+				/** ready to use. The `mongoose.connect()` promise resolves to mongoose instance. */
+				console.log('🚀 MongoDB is now connected.');
+			},
+			(err) => {
+				/** handle initial connection error */
+				console.log(
+					'🪫 MongoDB connection error. Please make sure MongoDB is running.',
+				);
+				process.exit();
+			},
+		);
+		// Use the Mongo driver's updated URL string parser
+		// mongoose.set('useNewUrlParser', true)
+		// Use findOneAndUpdate() in place of findAndModify()
+		// mongoose.set('useFindAndModify', false)
+		// Use createIndex() in place of ensureIndex()
+		// mongoose.set('useCreateIndex', true)
+		// Use the new server discovery and monitoring engine
+		// mongoose.set('useUnifiedTopology', true)
+		// Connect to the DB
+		// mongoose.connect(DB_HOST)
+		// Log an error if we fail to connect
+		// mongoose.connection.on('error', (err) => {
+		//   console.error(err)
+		//   console.log(
+		//     'MongoDB connection error. Please make sure MongoDB is running.'
+		//   )
+		//   process.exit()
+		// })
+	},
+	close: () => {
+		mongoose.connection.close();
+	},
+};
---
+++ apps/backend/src/models/index.js
@@ -0,0 +1,5 @@
+import Note from './note.js';
+const models = {
+	Note,
+};
+export default models;
