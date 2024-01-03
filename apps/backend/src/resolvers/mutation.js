bc9c70f120f0e2fb2257f31f8f7fd41d4142eb32
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2024-01-02 19:23:57 -0500

    🔐 Wired signup in mutation.js - excluded gravatar

--- apps/backend/src/resolvers/mutation.js
+++ apps/backend/src/resolvers/mutation.js
@@ -1,4 +1,11 @@
 // prettier-ignore
+
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
 export default {
 	newNote: async (parent, args, {models}) => {
 		return await models.Note.create({
@@ -18,19 +25,39 @@ export default {
 		try {
 			return await models.Note.findOneAndUpdate(
 				{
-					_id: id
+					_id: id,
 				},
 				{
 					$set: {
-						content
+						content,
 					},
 				},
 				{
-					new: true
+					new: true,
 				},
 			);
 		} catch (err) {
 			throw new Error('Error updating note');
 		}
-	}
+	},
+	signUp: async (parent, {username, email, password}, {models}) => {
+		// normalize email address
+		email = email.trim().toLowerCase();
+		// hash the password
+		const hashed = await bcrypt.hash(password, 10); // create the gravatar url
+		// const avatar = gravatar(email);
+		try {
+			const user = await models.User.create({
+				username,
+				email,
+				// avatar,
+				password: hashed,
+			});
+			// create and return the json web token
+			return jwt.sign({id: user._id}, process.env.JWT_SECRET);
+		} catch (err) {
+			console.log(err);
+			throw new Error('Error creating account');
+		}
+	},
 };
