0eea73ee9fbe6f45e6f77d1a07f5870b16c1055c
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2024-01-04 11:51:26 -0500

    ✅ Implemented User Queries

--- apps/backend/src/resolvers/query.js
+++ apps/backend/src/resolvers/query.js
@@ -5,4 +5,16 @@ export default {
 	note: async (parent, args, {models}) => {
 		return await models.Note.findById(args.id);
 	},
+	user: async (parent, {username}, {models}) => {
+		// find a user given their username
+		return await models.User.findOne({username});
+	},
+	users: async (parent, args, {models}) => {
+		// find all users
+		return await models.User.find({});
+	},
+	me: async (parent, args, {models, user}) => {
+		// find a user given the current user context
+		return await models.User.findById(user.id);
+	},
 };
--- apps/backend/src/schema.js
+++ apps/backend/src/schema.js
@@ -24,6 +24,9 @@ const typeDefs = gql`
 		hello: String
 		notes: [Note!]!
 		note(id: ID): Note!
+		user(username: String!): User
+		users: [User!]!
+		me: User!
 	}
 
 	type Mutation {
