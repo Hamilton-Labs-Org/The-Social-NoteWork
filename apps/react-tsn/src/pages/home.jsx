77308fdc5ec2f509cdbca3d100dfa31df41c5f11
Terence P. Hamilton <24661651+HamiltonMultimedia@users.noreply.github.com>
2024-01-15 18:07:14 -0500

    🚀 Added Apollo Client (#19)

--- apps/react-tsn/src/pages/home.jsx
+++ apps/react-tsn/src/pages/home.jsx
@@ -1,17 +1,47 @@
 import React, { useEffect } from "react";
 import Button from "../components/Button";
+import { useQuery, gql } from "@apollo/client";
 
 const Home = () => {
 	useEffect(() => {
 		// update the document title
 		document.title = "Home - NoteWork";
 	});
+	// our GraphQL query, stored as a variable
+	const GET_NOTES = gql`
+query NoteFeed($cursor: String) {
+        noteFeed(cursor: $cursor) {
+          cursor
+          hasNextPage
+          notes {
+            id
+            createdAt
+            content
+            favoriteCount
+            author {
+              username
+              id
+              avatar
+} }
+} }
+`;
+	// query hook
+	const { data, loading, error, fetchMore } = useQuery(GET_NOTES);
+	if (loading)
+		// if the data is loading, display a loading message
+		return <p>Loading...</p>;
+	// if there is an error fetching the data, display an error message if (error) return <p>Error!</p>;
 	return (
 		<>
 			<div>
 				<h2>This is the home page</h2>
 				{/* add a list of links here */}
 				<Button>Click me!</Button>
+			</div>
+			<div>
+				{data.noteFeed.notes.map((note) => (
+					<div key={note.id}>{note.author.username}</div>
+				))}
 			</div>
 		</>
 	);
