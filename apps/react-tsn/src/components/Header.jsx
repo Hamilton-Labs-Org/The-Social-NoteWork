a46a0a6fe6b5feff32cc5ab7700cae17ffa7a277
Terence P. Hamilton <24661651+HamiltonMultimedia@users.noreply.github.com>
2024-01-16 17:16:15 -0500

    🚀 Added Apollo Client (#20)
    * 🚀 Added Apollo Client
    
    * 📝 Updated README

--- apps/react-tsn/src/components/Header.jsx
+++ apps/react-tsn/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import logo from "../img/logo.svg";
+import logo from "../img/logo.png";
 
 const HeaderBar = styled.header` 
 	width: 100%;
