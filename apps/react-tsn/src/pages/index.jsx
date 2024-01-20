7c8d2a010d55224c6c6587760a91b1f0083a5926
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2024-01-20 11:42:16 -0500

    ✅ Added ProtectedRoute to Home Pages

--- apps/react-tsn/src/pages/index.jsx
+++ apps/react-tsn/src/pages/index.jsx
@@ -1,7 +1,6 @@
 // import React and routing dependencies
 import React from "react";
-import { Route, Routes, Navigate } from "react-router-dom";
-import { useQuery, gql } from "@apollo/client";
+import { Route, Routes } from "react-router-dom";
 
 // import ProtectedRoute component
 import ProtectedRoute from "../components/ProtectedRoute";
@@ -19,12 +18,6 @@ import Favorites from "./favorites";
 import NotePage from "./note";
 import SignUp from "./signup";
 import SignIn from "./signin";
-import { Component } from "react";
-
-const IS_LOGGED_IN = gql` {
-        isLoggedIn @client
-      }
-`;
 
 // define routes and export the module
 const Pages = () => {
@@ -40,20 +33,20 @@ const Pages = () => {
 						</ProtectedRoute>
 					}
 				/>
-				{/* <Route
+				<Route
 					path="/mynotes/*"
 					element={
-						<PrivateRoute redirectTo="/signin">
+						<ProtectedRoute isAllowed={isLoggedInVar()} redirectTo="/signin">
 							<MyNotes />
-						</PrivateRoute>
+						</ProtectedRoute>
 					}
-				/> */}
+				/>
 				<Route
 					path="/favorites/*"
 					element={
-						<PrivateRoute redirectTo="/signin">
+						<ProtectedRoute isAllowed={isLoggedInVar()} redirectTo="/signin">
 							<Favorites />
-						</PrivateRoute>
+						</ProtectedRoute>
 					}
 				/>
 				<Route path="/note/:id" element={<NotePage />} />
@@ -61,29 +54,6 @@ const Pages = () => {
 				<Route path="/signin" element={<SignIn />} />
 			</Routes>
 		</Layout>
-	);
-};
-
-// add the PrivateRoute component below our `Pages` component
-const PrivateRoute = ({ element, ...rest }) => {
-	const { loading, error, data } = useQuery(IS_LOGGED_IN); // if the data is loading, display a loading message
-	if (loading) return <p>Loading...</p>;
-	// if there is an error fetching the data, display an error message
-	if (error) return <p>Error!</p>;
-	// if the user is logged in, route them to the requested component // else redirect them to the sign-in page
-	return (
-		<Routes>
-			<Route
-				{...element}
-				render={(props) =>
-					data.isLoggedIn === true ? (
-						<Component {...element} />
-					) : (
-						<Navigate to="/signin" />
-					)
-				}
-			/>
-		</Routes>
 	);
 };
 
