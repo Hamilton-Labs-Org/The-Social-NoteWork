2bf3e00b0314bb172922719be6f8730c56ebfde5
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2024-01-22 10:42:17 -0500

    📝 Updated EditNote to use NoteForm

--- apps/react-tsn/src/pages/edit.jsx
+++ apps/react-tsn/src/pages/edit.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { useQuery, gql } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
+// import the GET_NOTE query
+import { GET_NOTE, GET_ME } from "../gql/query";
+import {} from "../gql/query";
 
 import { Link } from "react-router-dom";
 
@@ -11,15 +14,13 @@ import NoteForm from "../components/NoteForm";
 
 // the note query, which accepts an ID variable
 
-// import the GET_NOTE query
-import { GET_NOTE } from "../gql/query";
-
 // query hook, passing the id value as a variable
 
 const EditNote = (props) => {
 	const { id } = useParams();
 	const { loading, error, data } = useQuery(GET_NOTE, { variables: { id } });
-
+	// fetch the current user's data
+	const { data: userdata } = useQuery(GET_ME);
 	if (loading)
 		// if the data is loading, display a loading message
 
@@ -27,7 +28,13 @@ const EditNote = (props) => {
 	// if there is an error fetching the data, display an error message
 	if (error) return <p>Error! Note not found</p>;
 
-	return <Note note={data.note} />;
+	// if the current user and the author of the note do not match
+	if (userdata.me.id !== data.note.author.id) {
+		return <p>You do not have access to edit this note</p>;
+	}
+	// return <Note note={data.note} />;
+	// pass the data to the form component
+	return <NoteForm content={data.note.content} />;
 };
 
 export default EditNote;
