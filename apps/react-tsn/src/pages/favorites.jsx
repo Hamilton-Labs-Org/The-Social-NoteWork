5f944348be6a017ee7c1dfab331114946ccdf0fb
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2024-01-22 08:52:36 -0500

    ✅ Added favorites query to favorites.jsx

--- apps/react-tsn/src/pages/favorites.jsx
+++ apps/react-tsn/src/pages/favorites.jsx
@@ -1,16 +1,37 @@
 import React, { useEffect } from "react";
+import { useQuery, gql } from "@apollo/client";
+
+import NoteFeed from "../components/NoteFeed";
+// import the query
+import { GET_MY_FAVORITES } from "../gql/query";
 
 const Favorites = () => {
 	useEffect(() => {
 		// update the document title
 		document.title = "Favorites - NoteWork";
 	});
-	return (
-		<>
-			<div>
-				<h2>These are my favorites</h2>
-			</div>
-		</>
-	);
+
+	const { loading, error, data } = useQuery(GET_MY_FAVORITES);
+
+	if (loading)
+		// if the data is loading, our app will display a loading message
+		return "Loading...";
+	if (error)
+		// if there is an error fetching the data, display an error message
+		return `Error! ${error.message}`;
+	// if the query is successful and there are notes, return the feed of notes // else if the query is successful and there aren't notes, display a message
+	if (data.me.favorites.length !== 0) {
+		return <NoteFeed notes={data.me.favorites} />;
+	} else {
+		return <p>No favorites yet</p>;
+	}
+
+	// return (
+	// 	<>
+	// 		<div>
+	// 			<h2>These are my favorites</h2>
+	// 		</div>
+	// 	</>
+	// );
 };
 export default Favorites;
