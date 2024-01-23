d6110576fd7a25516f308f39feb3741b05b61e5d
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2024-01-22 19:34:19 -0500

    ⭐️ Created FavoriteNote component

---
+++ apps/react-tsn/src/components/FavoriteNote.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import ButtonAsLink from "./ButtonAsLink";
+const FavoriteNote = (props) => {
+	return <ButtonAsLink>Add to favorites</ButtonAsLink>;
+};
+export default FavoriteNote;
