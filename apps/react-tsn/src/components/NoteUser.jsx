a6b4c1ca9ef2b4c5a7bcdeb8800c21e6a93c4cc8
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2024-01-25 19:01:11 -0500

    🧹 Tidy up on NoteUser.jsx

--- apps/react-tsn/src/components/NoteUser.jsx
+++ apps/react-tsn/src/components/NoteUser.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useQuery, gql } from "@apollo/client";
+import { useQuery } from "@apollo/client";
 import { Link } from "react-router-dom";
 // import our GET_ME query
 import { GET_ME } from "../gql/query";
