7f1d402e0dbd6dd2dabafe6516af15ff80b888a1
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2024-01-26 20:14:33 -0500

    📋 Added PostHog

--- apps/react-tsn/src/main.tsx
+++ apps/react-tsn/src/main.tsx
@@ -4,13 +4,32 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./app/app";
 
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+
+// Uncomment the line belows if you want to use PostHog in development
+// posthog.init(import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_KEY, {
+// 	api_host: import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_HOST,
+// });
+
+if (
+	!window.location.host.includes("127.0.0.1") &&
+	!window.location.host.includes("localhost")
+) {
+	posthog.init(import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_KEY, {
+		api_host: import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_HOST,
+	});
+}
+
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement,
 );
 root.render(
 	<StrictMode>
-		<Router>
-			<App />
-		</Router>
+		<PostHogProvider client={posthog}>
+			<Router>
+				<App />
+			</Router>
+		</PostHogProvider>
 	</StrictMode>,
 );
