20325094a658a2e96ccba61530356742fb457aa0
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2024-01-31 20:35:10 -0500

    ✅ Added verify/token route to index

--- apps/backend/src/index.js
+++ apps/backend/src/index.js
@@ -96,6 +96,27 @@ app.use(
 	}),
 );
 
+app.get('/:id/verify/:token/', async (req, res) => {
+	try {
+		const user = await User.findOne({_id: req.params.id});
+		if (!user) return res.status(400).send({message: 'Invalid link'});
+
+		const token = await Token.findOne({
+			userId: user._id,
+			token: req.params.token,
+		});
+		if (!token)
+			return res.status(400).send({message: 'Invalid link'});
+
+		await User.updateOne({_id: user._id, verified: true});
+		await token.remove();
+
+		res.status(200).send({message: 'Email verified successfully'});
+	} catch (error) {
+		res.status(500).send({message: 'Internal Server Error'});
+	}
+});
+
 // Modified server startup
 await new Promise((resolve) =>
 	httpServer.listen({port: port}, resolve),
