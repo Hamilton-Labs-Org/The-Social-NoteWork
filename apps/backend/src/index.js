27eeb73d7ca02444c6618a048ae302d641eac340
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2024-01-31 22:19:12 -0500

    ✅ Updated index to use models.User & models.Token

--- apps/backend/src/index.js
+++ apps/backend/src/index.js
@@ -88,17 +88,17 @@ app.use(
 
 app.get('/:id/verify/:token/', async (req, res) => {
 	try {
-		const user = await User.findOne({_id: req.params.id});
+		const user = await models.User.findOne({id: req.params.id});
 		if (!user) return res.status(400).send({message: 'Invalid link'});
 
-		const token = await Token.findOne({
+		const token = await models.Token.findOne({
 			userId: models.User.id,
 			token: req.params.token,
 		});
 		if (!token)
 			return res.status(400).send({message: 'Invalid link'});
 
-		await User.updateOne({_id: user._id, verified: true});
+		await models.User.updateOne({id: user.id, verified: true});
 		await token.remove();
 
 		res.status(200).send({message: 'Email verified successfully'});
