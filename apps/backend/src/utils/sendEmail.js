47d90a719805118ba568baf2438c1c874749f6cf
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2024-01-31 21:30:46 -0500

    ✅ Added sendMail to utils

---
+++ apps/backend/src/utils/sendEmail.js
@@ -0,0 +1,29 @@
+import nodemailer from 'nodemailer';
+import 'dotenv/config';
+
+module.exports = async (email, subject, text) => {
+	try {
+		const transporter = nodemailer.createTransport({
+			host: process.env.HOST,
+			service: process.env.SERVICE,
+			port: Number(process.env.EMAIL_PORT),
+			secure: Boolean(process.env.SECURE),
+			auth: {
+				user: process.env.GMAIL_USER,
+				pass: process.env.GMAIL_PASS,
+			},
+		});
+
+		await transporter.sendMail({
+			from: process.env.GMAIL_USER,
+			to: email,
+			subject: subject,
+			text: text,
+		});
+		console.log('email sent successfully');
+	} catch (error) {
+		console.log('email not sent!');
+		console.log(error);
+		return error;
+	}
+};
