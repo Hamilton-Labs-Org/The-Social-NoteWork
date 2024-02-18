ddabbdad826726d2a5bc97d6cfe8ddc5dc0611a8
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2024-02-18 09:11:09 -0500

    🔐 Added reset Boolean to UserSchema. Fixes #31

--- apps/backend/src/models/user.js
+++ apps/backend/src/models/user.js
@@ -20,6 +20,10 @@ const UserSchema = new mongoose.Schema(
 			type: Boolean,
 			defaultValue: false,
 		},
+		reset: {
+			type: Boolean,
+			defaultValue: false,
+		},
 	},
 	{
 		// Assigns createdAt and updatedAt fields with a Date type
