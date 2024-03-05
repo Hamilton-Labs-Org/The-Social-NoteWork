97692db4f4db2a8ede9217f269407d804bb1a20a
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2024-03-05 09:24:18 -0500

    🔐 Fixing Password Reset system. Fixes #31

--- apps/react-tsn/src/pages/index.jsx
+++ apps/react-tsn/src/pages/index.jsx
@@ -18,60 +18,62 @@ import SignIn from './signin';
 import NewNote from './new';
 import EditNote from './edit';
 import EmailVerify from '../components/EmailVerify';
+import PasswordReset from '../components/PasswordReset';
 
 // define routes and export the module
 const Pages = () => {
-  return (
-    <Layout>
-      <Routes>
-        <Route
-          exact
-          path="/*"
-          element={
-            <ProtectedRoute isAllowed={isLoggedInVar()} redirectTo="/signin">
-              <Home />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/mynotes/*"
-          element={
-            <ProtectedRoute isAllowed={isLoggedInVar()} redirectTo="/signin">
-              <MyNotes />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/favorites/*"
-          element={
-            <ProtectedRoute isAllowed={isLoggedInVar()} redirectTo="/signin">
-              <Favorites />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/new/*"
-          element={
-            <ProtectedRoute isAllowed={isLoggedInVar()} redirectTo="/signin">
-              <NewNote />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/edit/:id/*"
-          element={
-            <ProtectedRoute isAllowed={isLoggedInVar()} redirectTo="/signin">
-              <EditNote />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/note/:id" element={<NotePage />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/users/:id/verify/:token" element={<EmailVerify />} />
-      </Routes>
-    </Layout>
-  );
+	return (
+		<Layout>
+			<Routes>
+				<Route
+					exact
+					path="/*"
+					element={
+						<ProtectedRoute isAllowed={isLoggedInVar()} redirectTo="/signin">
+							<Home />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path="/mynotes/*"
+					element={
+						<ProtectedRoute isAllowed={isLoggedInVar()} redirectTo="/signin">
+							<MyNotes />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path="/favorites/*"
+					element={
+						<ProtectedRoute isAllowed={isLoggedInVar()} redirectTo="/signin">
+							<Favorites />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path="/new/*"
+					element={
+						<ProtectedRoute isAllowed={isLoggedInVar()} redirectTo="/signin">
+							<NewNote />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path="/edit/:id/*"
+					element={
+						<ProtectedRoute isAllowed={isLoggedInVar()} redirectTo="/signin">
+							<EditNote />
+						</ProtectedRoute>
+					}
+				/>
+				<Route path="/note/:id" element={<NotePage />} />
+				<Route path="/signup" element={<SignUp />} />
+				<Route path="/signin" element={<SignIn />} />
+				<Route path="/reset" element={<PasswordReset />} />
+				<Route path="/users/:id/verify/:token" element={<EmailVerify />} />
+			</Routes>
+		</Layout>
+	);
 };
 
 export default Pages;
